ec0e9e6c01275277712167742647e1f517265824
Neeti <neeti.i.bhatt@gmail.com>
2023-12-15 15:21:46 -0800

    updating css file

--- starter_code/script.js
+++ starter_code/script.js
@@ -4,83 +4,82 @@ const suggestions = document.querySelector('.suggestions ul');
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 //constructor(){
-	//this.fruits=[...fruit];//assign list of fruit to fruits variable
-	//this.htmlData=document.querySelector("html"); //collecting html data
-	//this.bodyData=document.querySelector("body"); //collecting body data
+//this.fruits=[...fruit];//assign list of fruit to fruits variable
+//this.htmlData=document.querySelector("html"); //collecting html data
+//this.bodyData=document.querySelector("body"); //collecting body data
 
-	//this.inputVal=document.querySelector("#fruit");//get inputvalue from the searchbar
-	//this.suggestionsVal=document.querySelector(".suggestions ul");
+//this.inputVal=document.querySelector("#fruit");//get inputvalue from the searchbar
+//this.suggestionsVal=document.querySelector(".suggestions ul");
 
-	//this.inputVal.addEventListener("keyup",this.searchHandler.bind(this));//event would add when the key is pressed
-	//this.suggestionsVal=document.addEventListener("click",this.useSuggestion.bind(this));//event would add on click
-	//this.htmlData.addEventListener("click", this.useSuggestion.bind(this));//event would add on click
-   // this.bodyData.addEventListener("click", this.useSuggestion.bind(this));//event would add on click
+//this.inputVal.addEventListener("keyup",this.searchHandler.bind(this));//event would add when the key is pressed
+//this.suggestionsVal=document.addEventListener("click",this.useSuggestion.bind(this));//event would add on click
+//this.htmlData.addEventListener("click", this.useSuggestion.bind(this));//event would add on click
+// this.bodyData.addEventListener("click", this.useSuggestion.bind(this));//event would add on click
 //}
 
 function search(str) {
 	let results = [];
-	console.log("value of stre---->"+str+"\n\n\n");
+	console.log("value of stre---->" + str + "\n\n\n");
 	//if the strign is empty then return []
-	if(str.length===0)
+	if (str.length === 0)
 		return [];
-	
+
 	//converting str to lowercase for string matching
-	let newstr=str.toLowerCase();
+	let newstr = str.toLowerCase();
 	//search the friquent word and push it into result
-	fruit.filter((word)=>{
-		let newWord=word.toLowerCase();
-		let id=newWord.search(newstr);
-		if(id>-1)
-		{
+	fruit.filter((word) => {
+		let newWord = word.toLowerCase();
+		let id = newWord.search(newstr);
+		if (id > -1) {
 			results.push(word);
 		}
 	});
-	
+
 
 	return results;
 }
 
 function searchHandler(e) {
-	let result=search(input.value);
-	
+	let result = search(input.value);
+
 	//when the key is pressed in the input searchbar,showSuggetions and search function will be called
-	showSuggestions(result,input.value);
+	showSuggestions(result, input.value);
 }
 
 function showSuggestions(results, inputVal) {
-    suggestions.innerHTML = "";
+	suggestions.innerHTML = "";
 
-    results.forEach((word) => {
+	results.forEach((word) => {
 		//create li element
-      const li = document.createElement("li");
-      // Search, a word which is matched with fruit list thoese letter will become bold.
-      let bold = "<strong>" + word.slice(word.toLowerCase().indexOf(inputVal.toLowerCase())) + "</strong>";
-	  //enter the matched word into li
-      li.innerHTML = word.slice(0, word.toLowerCase().indexOf(inputVal.toLowerCase())) + bold;
-      // append li
-      suggestions.append(li);
-    });
-	
+		const li = document.createElement("li");
+		// Search, a word which is matched with fruit list thoese letter will become bold.
+		let bold = "<strong>" + word.slice(word.toLowerCase().indexOf(inputVal.toLowerCase())) + "</strong>";
+		//enter the matched word into li
+		li.innerHTML = word.slice(0, word.toLowerCase().indexOf(inputVal.toLowerCase())) + bold;
+		// append li
+		suggestions.append(li);
+	});
+
 }
 
 function useSuggestion(e) {
 	// by clicking outside inputbox and <li>, list will disappear.
-    if (
+	if (
 		e.target.tagName === "HTML" ||
 		e.target.tagName === "BODY" ||
 		e.target.tagName === "UL" ||
 		e.target.tagName === "INPUT"
-	  ) {
+	) {
 		suggestions.innerHTML = "";
 		// if click word as a value in <li>, suggestions.innerHTML's value will be matched to where you click.
-	  } else if (e.target.tagName === "LI") {
+	} else if (e.target.tagName === "LI") {
 		input.value = e.target.innerText;
 		suggestions.innerHTML = "";
 		// To see click on bold letter, input value should be matched to <li>'s value.
-	  } else if (e.target.tagName === "STRONG") {
+	} else if (e.target.tagName === "STRONG") {
 		input.value = e.target.closest("LI").innerText;
 		suggestions.innerHTML = "";
-	  }
+	}
 }
 
 input.addEventListener('keyup', searchHandler);
